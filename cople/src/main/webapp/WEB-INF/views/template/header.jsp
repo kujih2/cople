5a88ddd5c680ad0a989abe5374835a17031fda34
Kyeonwoo Lim <Kyeonwoo Lim@kyeonwoo>
2024-01-20 11:19:50 +0900

    헤더변경

--- cople/src/main/webapp/WEB-INF/views/template/header.jsp
+++ cople/src/main/webapp/WEB-INF/views/template/header.jsp
@@ -4,7 +4,9 @@
 <!-- 상단시작 -->
 <h2 class="align-center">SpringPage</h2>
 <div class="align-right">
-<a href="${pageContext.request.contextPath}/board/list">게시판</a>
+	
+	<a href="${pageContext.request.contextPath}/wiki/detail">위키</a>
+	<a href="${pageContext.request.contextPath}/board/list">게시판</a>
 	<c:if test="${!empty user}">
 		<a href="${pageContext.request.contextPath}/member/myPage">MY페이지</a>
 		<img src="${pageContext.request.contextPath}/member/photoView" width="25" height="25" class="my-photo">
---
+++ cople/src/main/webapp/sql/LKW.sql
@@ -0,0 +1,21 @@
+--	문서 테이블
+create table wiki_doc(
+	doc_num number not null,
+	doc_name varchar2(150) unique not null,
+	constraint wiki_doc_pk primary key(doc_num)
+);
+create sequence wiki_doc_seq;
+--업데이트 테이블
+create table wiki_update(
+	doc_num number not null,
+	update_num number not null,
+	update_content blob,
+	doc_status number not null,
+	update_status number default 1 not null,
+	update_writer number not null,
+	update_summary varchar2(300),
+	update_date date default sysdate not null,
+	constraint wiki_update_pk primary key (update_num),
+	constraint wiki_update_fk foreign key (doc_num) references wiki_doc (doc_num)
+);
+create sequence wiki_update_seq;
