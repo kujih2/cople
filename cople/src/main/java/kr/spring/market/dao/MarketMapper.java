76344a88df5c538f4abc886130c6446b6bd28b97
User <User@DESKTOP-18KC9KO>
2024-02-05 00:44:36 +0900

    marketchat 작업

--- cople/src/main/java/kr/spring/market/dao/MarketMapper.java
+++ cople/src/main/java/kr/spring/market/dao/MarketMapper.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-
+import kr.spring.market.vo.MarketChatRoomVO;
+import kr.spring.market.vo.MarketChatVO;
 import kr.spring.market.vo.MarketVO;
+
 
 @Mapper
 public interface MarketMapper {
@@ -19,4 +21,11 @@ public interface MarketMapper {
     public void deleteFile(int product_num);
 	//관심목록
    //채팅
+    public List<MarketVO> selectChatRoomList(Map<String,Object> map); //채팅방 조회
+    public void insertChatRoom(MarketChatRoomVO chatRoomVO); //채팅방 생성
+  	public int selectchatRoomNum(); //채팅방 번호 생성
+ 
+
+  	public int selectTalkNum(); //채팅 메시지 번호 생성
+  	public void insertTalk(MarketChatVO chatVO);//채팅 메시지 등록
 }
